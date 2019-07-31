c1feddc8f615031171b0f2d930a3bce70279c90d
prudhvireddy321 <40226166+prudhvireddy321@users.noreply.github.com>
2019-07-31 17:58:17 +0530

    Update dli.py

--- dli.py
+++ dli.py
@@ -1,11 +1,11 @@
 #a
-p = int(input())
+o = int(input())
 q = [ int(x) for x in input().split()]
-p = len(q)
+o = len(q)
 u = 0
-for i in range(0,p-2):
-    for j in range(i+1, p-1):
-        for k in range(j+1, p):
+for i in range(0,o-2):
+    for j in range(i+1, o-1):
+        for k in range(j+1, o):
             if q[i] > q[j] > q[k] :
                 u =u+ 1
 print(u)
